d793b573c896231cc289d6e6c16c04a6b6f5157e
Yancey Leo <yanceyofficial@gmail.com>
2019-07-02 17:39:15 +0800

    feat: Learn css in js

--- src/containers/CSSInJS.tsx
+++ src/containers/CSSInJS.tsx
@@ -1,10 +1,5 @@
-import React from 'react';
+import React, { ReactElement, ReactComponentElement } from 'react';
 import styled from 'styled-components';
-
-const P = styled.p`
-  font-size: 20px;
-  color: blue;
-`;
 
 interface ICSSInJSProps {
   primary?: boolean;
@@ -17,25 +12,50 @@ class CSSInJS extends React.Component<ICSSInJSProps, {}> {
   }
 
   public render() {
+    // 基本玩法
+    const H1 = styled.h1`
+      font-size: 20px;
+      color: blue;
+      text-align: center;
+    `;
+
+    // 传入参数
     const Button = styled.button`
       background: ${(props: ICSSInJSProps) =>
         props.primary ? 'palevioletred' : 'white'};
       color: ${(props: ICSSInJSProps) =>
         props.primary ? 'white' : 'palevioletred'};
-
       font-size: 1em;
       margin: 1em;
-      padding: 0.25em 1em;
+      padding: 0.5em 1em;
       border: 2px solid palevioletred;
       border-radius: 3px;
       outline: none;
       cursor: pointer;
     `;
+
+    // 继承
+    const TomatoButton = styled(Button)`
+      border-color: tomato;
+      color: tomato;
+    ` as any;
+
+    const ReversedButton = (props: any) => (
+      <button {...props} children={props.children.split('').reverse()} />
+    );
     return (
       <div>
-        <P>尝试 styled-component</P>
-        <Button primary>primary</Button>
-        <Button>default</Button>
+        <H1>Learn styled-component</H1>
+        <Button primary>Primary</Button>
+        <Button>Normal</Button>
+        <TomatoButton>TomatoButton</TomatoButton>
+        {/* 通过 as 将组件转换到某元素 */}
+        <TomatoButton as='a' href='/'>
+          实际是一个 a 标签
+        </TomatoButton>
+
+        {/* 通过 as 将组件转换成某元素 */}
+        <TomatoButton as={ReversedButton}>TomatoButton</TomatoButton>
       </div>
     );
   }
