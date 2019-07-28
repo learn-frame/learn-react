467186774ec0663162572fbd82a55bb381ceb300
Yancey <developer@yanceyleo.com>
2019-07-28 22:44:13 +0800

    feat: Learn Hooks

--- src/containers/Hooks.tsx
+++ src/containers/Hooks.tsx
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 
 function Hooks() {
   const [count, setCount] = useState(0);
+  // 当累加到 10 的时候清零
+  const handleCountChange = (count: number) => {
+    count === 10 ? setCount(0) : setCount(count + 1)
+  }
   return (
     <div>
       <p>我为长者+{count}s</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => handleCountChange(count)}>Click me</button>
     </div>
   );
 }
