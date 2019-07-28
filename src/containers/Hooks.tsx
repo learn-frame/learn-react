4ed07f37264833f3dd7ca174c7b529c42853dca7
Yancey <developer@yanceyleo.com>
2019-07-28 22:53:54 +0800

    feat: Add types

--- src/containers/Hooks.tsx
+++ src/containers/Hooks.tsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-function Hooks() {
-  const [count, setCount] = useState(0);
+interface Props {
+  children: React.ReactNode
+}
+
+const Hooks: React.FC<Props> = props => {
+  const [count, setCount] = useState(0)
   // 当累加到 10 的时候清零
   const handleCountChange = (count: number) => {
     count === 10 ? setCount(0) : setCount(count + 1)
   }
+
+  const { children } = props
   return (
     <div>
+      {children}
       <p>我为长者+{count}s</p>
       <button onClick={() => handleCountChange(count)}>Click me</button>
     </div>
-  );
+  )
 }
 
-export default Hooks;
+export default Hooks
