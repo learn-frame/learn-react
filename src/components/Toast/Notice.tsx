e8b30221d91f4358a9b8de8384bb296fa1b849a4
Yancey Leo <yanceyofficial@gmail.com>
2019-12-08 22:33:13 +0800

    feat: update Toast component

--- src/components/Toast/Notice.tsx
+++ src/components/Toast/Notice.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import './Toast.css';
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import './Toast.css'
 
 interface INoticeProps {
-  type: string;
-  content: string;
+  type: string
+  content: string
 }
 
 const iconList: { [key: string]: IconProp } = {
@@ -13,19 +13,19 @@ const iconList: { [key: string]: IconProp } = {
   loading: 'spinner',
   warning: 'exclamation-circle',
   error: 'times-circle',
-  info: 'info-circle'
-};
+  info: 'info-circle',
+}
 
 class Notice extends React.Component<INoticeProps, {}> {
   render() {
-    const { type, content } = this.props;
+    const { type, content } = this.props
     return (
       <div className={`toast-notice ${type}`}>
         <FontAwesomeIcon icon={iconList[type]} />
         <span>{content}</span>
       </div>
-    );
+    )
   }
 }
 
-export default Notice;
+export default Notice
