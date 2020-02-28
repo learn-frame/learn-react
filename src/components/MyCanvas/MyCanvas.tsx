e236438f38f980b0040e2bf11d336bfeb3ed2d38
Yancey Leo <yanceyofficial@gmail.com>
2020-02-28 10:27:36 +0800

    feat: optimization codes

--- src/components/MyCanvas/MyCanvas.tsx
+++ src/components/MyCanvas/MyCanvas.tsx
@@ -5,7 +5,38 @@ import correntIcon from './correct.png'
 const MyCanvas: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
 
-  function drawImg(img: HTMLImageElement) {
+  const renderIcon = (ctx: CanvasRenderingContext2D, x: number, y: number) => {
+    const icon = new Image()
+    icon.src = correntIcon
+    icon.onload = function() {
+      ctx.drawImage(
+        icon,
+        x * window.devicePixelRatio,
+        y * window.devicePixelRatio,
+        20 * window.devicePixelRatio,
+        20 * window.devicePixelRatio,
+      )
+    }
+  }
+
+  const renderImg = (ctx: CanvasRenderingContext2D) => {
+    const img = new Image()
+    img.src = bg
+
+    img.onload = function() {
+      ctx.drawImage(
+        img,
+        0,
+        0,
+        window.innerWidth * window.devicePixelRatio,
+        window.innerHeight * window.devicePixelRatio,
+      )
+
+      renderIcon(ctx, 140, 50)
+    }
+  }
+
+  useEffect(() => {
     if (canvasRef && canvasRef.current) {
       const ctx = canvasRef.current.getContext('2d')
 
@@ -16,53 +47,9 @@ const MyCanvas: FC = () => {
       canvasRef.current.height = window.innerHeight * window.devicePixelRatio
 
       if (ctx) {
-        ctx.drawImage(
-          img,
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height,
-        )
+        renderImg(ctx)
       }
     }
-  }
-
-  function drawIcon(icon: HTMLImageElement) {
-    if (canvasRef && canvasRef.current) {
-      const ctx = canvasRef.current.getContext('2d')
-
-      if (ctx) {
-        ctx.drawImage(
-          icon,
-          140 * window.devicePixelRatio,
-          50 * window.devicePixelRatio,
-          20 * window.devicePixelRatio,
-          20 * window.devicePixelRatio,
-        )
-      }
-    }
-  }
-
-  const renderIcon = () => {
-    const img = new Image()
-    img.src = correntIcon
-    img.onload = function() {
-      drawIcon(img)
-    }
-  }
-
-  const renderImg = () => {
-    const img = new Image()
-    img.src = bg
-
-    img.onload = function() {
-      drawImg(img)
-      renderIcon()
-    }
-  }
-
-  useEffect(() => {
-    renderImg()
   }, [])
 
   return (
