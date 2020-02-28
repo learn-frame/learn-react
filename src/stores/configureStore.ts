89345b04bc29575dc97d0bf43ca1071a0230a1a6
Yancey Leo <yanceyofficial@gmail.com>
2020-02-28 08:05:08 +0800

    feat: upadte configureStore

--- src/stores/configureStore.ts
+++ src/stores/configureStore.ts
@@ -1,34 +1,42 @@
-import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './rootReducer';
-import rootSaga from './rootSaga';
+import { applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './rootReducer'
+import rootSaga from './rootSaga'
 
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 
 export default function configureStore(initialState?: {}) {
   // 收集中间件
-  const middlewares = [sagaMiddleware, logger];
+  const middlewares = [sagaMiddleware, logger]
 
   // 应用中间件
-  const middlewareEnhancer = applyMiddleware(...middlewares);
+  const middlewareEnhancer = applyMiddleware(...middlewares)
 
   // 收集增强，增强可以包含中间件和一些手写的增强
-  const enhancers = [middlewareEnhancer];
+  const enhancers = [middlewareEnhancer]
 
   // 组合增强
-  const composedEnhancers = composeWithDevTools(...enhancers);
+  const composedEnhancers = composeWithDevTools(...enhancers)
 
-  const store = createStore(rootReducer, initialState, composedEnhancers);
+  const store = createStore(rootReducer, initialState, composedEnhancers)
 
-  sagaMiddleware.run(rootSaga);
+  sagaMiddleware.run(rootSaga)
 
   if (process.env.NODE_ENV !== 'production' && (module as any).hot) {
-    (module as any).hot.accept('./rootReducer', () =>
+    ;(module as any).hot.accept('./rootReducer', () =>
       store.replaceReducer(rootReducer),
-    );
+    )
   }
 
-  return store;
+  return store
 }
+
+// const store = configureStore()
+// store.dispatch()
+// store.replaceReducer()
+
+// 以下都不能在调度过程中使用
+// store.subscribe()
+// store.getState()
