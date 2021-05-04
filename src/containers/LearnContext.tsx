2082124331172b42872d0012fd31c3f9b029053d
YanceyOfficial <yanceyofficial@gmail.com>
2021-05-04 23:59:31 +0800

    feat: update

--- src/containers/LearnContext.tsx
+++ src/containers/LearnContext.tsx
@@ -2,15 +2,20 @@ import { FC, useContext } from 'react'
 import { ThemeContext } from 'src'
 
 const LearnContext: FC = () => {
+  // 可以通过传统的 Consumer 来订阅 context
   const ThemeConsumer = ThemeContext.Consumer
+
+  // 也可以用 Hooks
   const context = useContext(ThemeContext)
+
+  console.log(ThemeContext)
   return (
     <ThemeConsumer>
       {(value) => {
         return (
           <>
-            <div>ThemeConsumer: {value}</div>
-            <div>context: {context}</div>
+            <div>uses Consumer component: {value}</div>
+            <div>uses useContext hooks: {context}</div>
           </>
         )
       }}
