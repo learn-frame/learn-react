cf67655fb013ad6fb7bf5727dcf069ebd422dfe6
YanceyOfficial <yanceyofficial@gmail.com>
2022-09-13 23:47:42 +0800

    feat: add wasm

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Layouts from './layouts'
 import './index.css'
 
+if (process.env.NODE_ENV !== 'production') {
+  import('vconsole').then((m) => new m.default())
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Layouts />
